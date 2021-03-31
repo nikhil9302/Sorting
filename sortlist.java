3659d0a7ad540615f6dca198150440bccb957f4e
Nikhil Menon <74824711+nikhil9302@users.noreply.github.com>
2021-03-31 10:53:33 +0530

    Create sortlist.java

---
+++ sortlist.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+public class Main {  
+public static void main(String[] args) {  
+    List<String> slist = Arrays.asList("Wind", "In", "Our", "Chest", "Hair", "LHD");  
+System.out.println("List : "+slist);
+Collections.sort(slist);  
+System.out.println("Sorted List : "+slist);  
+    }  
+}
